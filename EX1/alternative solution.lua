89a14c7c5b248fcefa8bbf01a21137fc2faccded
Aanok <fabrizio07@gmail.com>
2016-10-13 10:16:42 +0200

    Updated with a decent explanation. Cleaned up and corrected the algorithm: previous version was actually a linear time solution (oops).

--- EX1/alternative solution.lua
+++ EX1/alternative solution.lua
@@ -1,29 +1,51 @@
 --[[
-  In this solution we employ an explicit tree structure.
+  In this solution we employ an explicit tree structure, a variant of a segment tree.
   We will limit ourselves to arrays whose length is a power of two. A generalization could be
   made by splitting an arbitrarily sized array in runs as long as power of two's, then building 
   and managing a corresponding forest of trees: the asymptotic performance would not be affected.
-  The trees could also be made implicit and all manipulations done on an array, with due care; this
-  would however make the algorithm even harder to understand than it already is, so fuck that.
+  The trees could also be made implicit and all manipulations done on an array (heap-like), with due
+  care; this would however make the algorithm even harder to understand than it already is, so let's not.
   
   We will represent an array of N elements as a complete binary tree with N leaves, for a total of
   2*N - 1 nodes. Each node will possess the following fiels:
-    - psum: something that resembles a partial sum of all descendants, but is not quite so (it's tricky);
+    - psum: the partial sum of all indexed (descendant) array elements;
     - label: the algebric sum of all constants added in update operations that terminated at the node;
     - min: the index of the leftmost leaf descending from the node;
     - max: the index of the rightmost leaf descending from the node;
     - left: the left child of the node;
     - right: the right child of the node.
-    (psum and label are the foundations, the rest is for conveniency)
+    (psum and label are the fundamentals, everything else is for conveniency)
   
   The general intuition is to split update intervals into sub-intervals whose length is a power of two.
-  Each sub-interval corresponds to a node. Whenever we do an update operation, we stop as high up in
-  the tree as possible, i.e. at the biggest sub-intervals we can. When we stop, we annotate the node
-  by updating its label field with the algebric sum of the update constant and the current label. When
-  we visit a node (passing through or stopping), we likewise update its psum field with the algebric sum
-  of the update constant and the current psum.
+  Each sub-interval corresponds to a node (leaves are singleton intervals).
   
-  When computing sum()... [TODO, just look at the code for now]
+  Suppose we run update(i, j, c). We start traversing the tree depth-first, reaching all nodes whose
+  sub-intervals are interested by the update interval. On each branch we stop as high up as possible,
+  i.e. at the biggest sub-interval we can, which means at the first node n such that [n.min, n.max] is
+  wholly contained in [i, j].
+  When we visit a node (passing through or stopping), we update its psum field by 
+  
+  psum = psum + c * #([i, j]n[n.min, n.max]).
+  {psum increased by c times the cardinality of the intersection of [i,j] and [n.min,n.max].
+  ASCII maths is a wild ride.}
+  
+  That is, we add c times the amount of leaves (array elements) below the node interested by the update
+  operation, which is the change in the partial sum relative to n's sub-interval.
+  If n is a node we stop at, we also update its label field by
+  
+  label = label + c.
+  
+  That is, we annotate the contribution to the value of each leaf (array element) given by each sub-interval.
+  
+  In fact, suppose we run sum(0, i). We start traversing the tree depth-first, again reaching all the nodes
+  whose sub-intervals intersect with the sum interval.
+  Whenever we reach a node whose sub-interval is wholly contained in the sum interval, we return its partial
+  sum. When we just traverse a node, we increase our return value by the node's label times the ardinality of
+  the intersection of the sum interval and the node's sub-interval, i.e. the amount of leaves below the node
+  that are interested by the update operation).
+  
+  The query on the value of a single element then becomes trivial: navigate to the corresponding node, summing
+  the labels of all nodes along the path.
 ]]--
 
 intersection = function(s1, e1, s2, e2) 
@@ -36,9 +58,10 @@ intersection = function(s1, e1, s2, e2)
 end
 
 update = function(n, i, j, c)
-  n.psum = n.psum + c * intersection(i, j, n.min, n.max)
-  if (n.min == n.max) then return end -- leaf node
-  if (n.min == i and n.max == j) then -- the update interval fits perfectly below
+  local int = intersection(i, j, n.min, n.max)
+  n.psum = n.psum + c * int
+  if (int == n.max - n.min + 1) then
+    -- the update interval covers our whole subtree
     n.label = n.label + c
     return
   end
@@ -50,18 +73,19 @@ update = function(n, i, j, c)
   end
 end
 
-sum = function(n, i)
---[[
-  To compute this over an [x1, x2] interval you'd call
-  sum(n, x2) - sum(n, x1). I'm not bothering.
---]]
-  if (i >= n.max) then return n.psum end
+sum = function(n, i, j)
   local int, tmp
+  int = intersection(i, j, n.min, n.max)
+  if (int == n.max - n.min + 1) then
+    -- the sum interval covers our whole subtree
+    return n.psum
+  end
   tmp = 0
   for _,d in pairs({ n.left, n.right }) do
-    int = intersection(0, i, d.min, d.max)
+    int = intersection(i, j, d.min, d.max)
     if (int > 0) then
-      tmp = tmp + (n.label * int) + sum(d, i)
+      -- the sum interval involves the child, so we go there
+      tmp = tmp + (n.label * int) + sum(d, i, j)
     end
   end
   return tmp
@@ -70,7 +94,7 @@ end
 query = function(n, i)
   local cur = n
   local tmp = 0
-  while (cur.min ~= i or cur.max ~= i) do
+  while (cur ~= nil) do
     tmp = tmp + cur.label
     if (i <= (cur.max + cur.min)/2) then
       cur = cur.left
@@ -78,13 +102,13 @@ query = function(n, i)
       cur = cur.right
     end
   end
-  return tmp + cur.psum
+  return tmp
   --[[
   Alternatively, with same asymptotic performance
   if i == 0 then
-    return sum(n, 0)
+    return sum(n, 0, 0)
   else
-    return sum(n, i) - sum(n, i-1)
+    return sum(n, 0, i) - sum(n, 0, i-1)
   end
   --]]
 end
@@ -110,7 +134,7 @@ end
 
 --[[
 ++++ DEMO ++++
-We'll run the same update and sum operations on an array of integers and the corresponding tree.
+We'll run the same update, sum and query operations on an array of integers and the corresponding tree.
 We'll see that they match for large, randomized sequences of operations (which is not a proof but
 still reassuring).
 --]]
@@ -142,9 +166,9 @@ update_array = function(a, i, j, c)
   end
 end
 
-sum_array = function(a, i)
+sum_array = function(a, i, j)
   local retval = 0
-  for t = 0, i do
+  for t = i, j do
     retval = retval + a[t]
   end
   return retval
@@ -161,12 +185,16 @@ do
     x1 = math.random(0, size-1)
     x2 = math.random(x1, size-1)
     c = math.random(-100, 100)
-    t = math.random(0, size-1)
     update(tree, x1, x2, c)
     update_array(array, x1, x2, c)
-    s1 = sum(tree, t)
-    s2 = sum_array(array, t)
-    print("Sum("..t.."): tree: "..s1..", array: "..s2)
+    x1 = math.random(0, size-1)
+    x2 = math.random(x1, size-1)
+    s1 = sum(tree, x1, x2)
+    s2 = sum_array(array, x1, x2)
+    print("Sum("..x1..","..x2.."): tree: "..s1..", array: "..s2)
     assert(s1 == s2)
+    s1 = query(tree, x1)
+    print("Query("..x1.."): tree: "..s1..", array: "..array[x1])
+    assert(s1 == array[x1])
   end
 end
